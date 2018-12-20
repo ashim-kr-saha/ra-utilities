bb0e1b966a9354ff43979af025e91c8b3e3cd0b1
Ashim Saha <ashimsaha2311@gmail.com>
2018-12-20 22:43:32 +0530

    Change for visible

--- src/CreateRelatedResourceButton/index.js
+++ src/CreateRelatedResourceButton/index.js
@@ -3,31 +3,35 @@ import { Link } from 'react-admin';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
-const CreateRelatedResourceButton = ({ actionName, record, newResource, fieldsMap, condition }) => {
+const CreateRelatedResourceButton = ({ actionName, record, newResource, fieldsMap, condition, visible }) => {
   var fields = Object.keys(fieldsMap);
   var newRecord = {};
   for(var i = 0; i < fields.length; i++) {
     var field = fields[i];
     newRecord[field] = record[fields[field]];
   }
-
-  return (
-    <Button
-      component={Link}
-      to={{
-        pathname: `/${newResource}/create`,
-        state: { record: newRecord },
-      }}
-      disabled={!condition(record)}
-    >
-      {actionName}
-  </Button>
-  )
+  if(visible(record)) {
+    return (
+      <Button
+        component={Link}
+        to={{
+          pathname: `/${newResource}/create`,
+          state: { record: newRecord },
+        }}
+        disabled={!condition(record)}
+      >
+        {actionName}
+      </Button>
+    )
+  } else {
+    return null;
+  }
 }
 
 CreateRelatedResourceButton.propTypes = {
   actionName: PropTypes.string,
   record: PropTypes.object,
+  visible: PropTypes.func,
   condition: PropTypes.func,
   fieldsMap: PropTypes.object,
   newResource: PropTypes.string,
